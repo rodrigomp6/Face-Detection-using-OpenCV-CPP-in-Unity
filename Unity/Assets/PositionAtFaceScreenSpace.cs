c0d10f25f0027df6371423792422a45cb52a6e78
rodrigomp6 <rodrigomp6@gmail.com>
2021-03-30 20:12:41 -0300

    Cena teste

--- Unity/Assets/PositionAtFaceScreenSpace.cs
+++ Unity/Assets/PositionAtFaceScreenSpace.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class PositionAtFaceScreenSpace : MonoBehaviour
 {
     private float _camDistance;
+    private Vector3 lastHeadPos;
+    private float maxDistance;
+
 
     void Start()
     {
@@ -13,13 +16,17 @@ public class PositionAtFaceScreenSpace : MonoBehaviour
     {
         if (OpenCVFaceDetection.NormalizedFacePositions.Count == 0)
             return;
-
         //transform.position = Camera.main.ViewportToWorldPoint(new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x, OpenCVFaceDetection.NormalizedFacePositions[0].y, OpenCVFaceDetection.NormalizedFacePositions[0].z));
-
         Vector3 headpos = new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y*-20, OpenCVFaceDetection.NormalizedFacePositions[0].z);
-
-
+        var distance = Vector3.Distance(lastHeadPos, headpos);
+        lastHeadPos = headpos;
+        if (distance > maxDistance)
+        {
+            maxDistance = distance;
+            Debug.Log("New max distance acheved:" + maxDistance);
+        }
         transform.position = Vector3.Lerp(transform.position, headpos, Time.deltaTime);
 
+        
     }
 }
