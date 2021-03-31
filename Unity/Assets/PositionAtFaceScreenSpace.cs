d0ff4d5d56a38dff1bc41b9d4fec706958669bdc
rodrigomp6 <rodrigomp6@gmail.com>
2021-03-31 16:40:43 -0300

    Aplicação face detection no gameplay

--- Unity/Assets/PositionAtFaceScreenSpace.cs
+++ Unity/Assets/PositionAtFaceScreenSpace.cs
@@ -3,27 +3,23 @@ using UnityEngine;
 
 public class PositionAtFaceScreenSpace : MonoBehaviour
 {
-    //private float _camDistance;
     private readonly float[] distances = new float[600];
     private int distancesArrayPos = 0;
     private float averageDistance = 0;
-
-
-
     private Vector3 lastHeadPos;
-    //private float maxDistance;
 
 
     void Start()
     {
+        
         //_camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
     }
 
     void Update()
     {
-        if (OpenCVFaceDetection.NormalizedFacePositions.Count == 0)
+/*        if (OpenCVFaceDetection.NormalizedFacePositions.Count == 0)
             return;
-        //transform.position = Camera.main.ViewportToWorldPoint(new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x, OpenCVFaceDetection.NormalizedFacePositions[0].y, OpenCVFaceDetection.NormalizedFacePositions[0].z));
+*/        //transform.position = Camera.main.ViewportToWorldPoint(new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x, OpenCVFaceDetection.NormalizedFacePositions[0].y, OpenCVFaceDetection.NormalizedFacePositions[0].z));
         Vector3 headpos = new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y*-20, OpenCVFaceDetection.NormalizedFacePositions[0].z*-20);
         transform.position = Vector3.Lerp(transform.position, headpos, Time.deltaTime);
         var distance = Vector3.Distance(lastHeadPos, transform.position);
@@ -31,7 +27,7 @@ public class PositionAtFaceScreenSpace : MonoBehaviour
 
         float variation = Math.Abs(distance * averageDistance);
         if (variation >= 0.5)
-            Debug.Log("fright: " + variation * 100 + "%");
+            Debug.Log("fright: " + variation);
 
         distances[distancesArrayPos] = distance;
         if (distancesArrayPos >= 599)
@@ -39,9 +35,12 @@ public class PositionAtFaceScreenSpace : MonoBehaviour
         else
             distancesArrayPos++;
         averageDistance = getAverage();
+
+        
+
         //Debug.Log("Array lenght: " + distances.Length);
         //Debug.Log("Average distance: " + averageDistance);
-       
+        
     }
 
     private float getAverage()
