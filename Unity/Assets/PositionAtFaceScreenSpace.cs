4fcc47b63e42866783cce780290036f541fabf1d
rodrigomp6 <rodrigomp6@gmail.com>
2021-03-30 23:27:42 -0300

    Algoritmo de detecção de susto do jogador

--- Unity/Assets/PositionAtFaceScreenSpace.cs
+++ Unity/Assets/PositionAtFaceScreenSpace.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 
 public class PositionAtFaceScreenSpace : MonoBehaviour
 {
-    private float _camDistance;
+    //private float _camDistance;
+    private readonly float[] distances = new float[600];
+    private int distancesArrayPos = 0;
+    private float averageDistance = 0;
+
+
+
     private Vector3 lastHeadPos;
-    private float maxDistance;
+    //private float maxDistance;
 
 
     void Start()
     {
-        _camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
+        //_camDistance = Vector3.Distance(Camera.main.transform.position, transform.position);
     }
 
     void Update()
@@ -17,16 +24,34 @@ public class PositionAtFaceScreenSpace : MonoBehaviour
         if (OpenCVFaceDetection.NormalizedFacePositions.Count == 0)
             return;
         //transform.position = Camera.main.ViewportToWorldPoint(new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x, OpenCVFaceDetection.NormalizedFacePositions[0].y, OpenCVFaceDetection.NormalizedFacePositions[0].z));
-        Vector3 headpos = new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y*-20, OpenCVFaceDetection.NormalizedFacePositions[0].z);
-        var distance = Vector3.Distance(lastHeadPos, headpos);
-        lastHeadPos = headpos;
-        if (distance > maxDistance)
-        {
-            maxDistance = distance;
-            Debug.Log("New max distance acheved:" + maxDistance);
-        }
+        Vector3 headpos = new Vector3(OpenCVFaceDetection.NormalizedFacePositions[0].x*-20, OpenCVFaceDetection.NormalizedFacePositions[0].y*-20, OpenCVFaceDetection.NormalizedFacePositions[0].z*-20);
         transform.position = Vector3.Lerp(transform.position, headpos, Time.deltaTime);
+        var distance = Vector3.Distance(lastHeadPos, transform.position);
+        lastHeadPos = transform.position;
 
+        float variation = Math.Abs(distance * averageDistance);
+        if (variation >= 0.5)
+            Debug.Log("fright: " + variation * 100 + "%");
+
+        distances[distancesArrayPos] = distance;
+        if (distancesArrayPos >= 599)
+            distancesArrayPos = 0;
+        else
+            distancesArrayPos++;
+        averageDistance = getAverage();
+        //Debug.Log("Array lenght: " + distances.Length);
+        //Debug.Log("Average distance: " + averageDistance);
+       
+    }
+
+    private float getAverage()
+    {
+        if (distances.Length <= 0)
+            return 0;
         
+        float sum = 0;
+        for (int i = 0; i < distances.Length; i++)
+            sum += distances[i];
+        return sum / distances.Length;
     }
 }
